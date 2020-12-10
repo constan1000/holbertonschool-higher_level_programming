fe48a6ec5bec4249c97e0e8cb537761f3242fc7d
constan1000 <hrairimejdi99@gmail.com>
2020-12-10 23:07:47 +0100

    hard

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
 from sys import argv
-
+from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
+            avlen = len(argv)
 
-        if len(argv) != 4:
-                    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-                            exit(1)
-                                a = int(argv[1])
-                                    b = int(argv[3])
-                                        if argv[2] == '+':
-                                                    print("{} + {} = {}".format(a, b, add(a, b)))
-                                        elif argv[2] == '-':
-                                                    print("{} - {} = {}".format(a, b, sub(a, b)))
-                                        elif argv[2] == '*':
-                                                    print("{} * {} = {}".format(a, b, mul(a, b)))
-                                        elif argv[2] == '/':
-                                                    print("{} / {} = {}".format(a, b, div(a, b)))
-                                        else:
-                                                    print("Unknown operator. Available operators: +, -, * and /")
-                                                            exit(1)
+                if avlen != 4:
+                                print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+                                        exit(1)
+
+                                            sign = str(argv[2])
+                                                if sign != '+' and sign != '-' and sign != '/' and sign != '*':
+                                                                print("Unknown operator. Available operators: +, -, * and /")
+                                                                        exit(1)
+
+                                                                            a = int(argv[1])
+                                                                                b = int(argv[3])
+
+                                                                                    if sign == '+':
+                                                                                                    print("{} + {} = {}" .format(a, b, add(a, b)))
+
+                                                                                    elif sign == '-':
+                                                                                                    print("{} - {} = {}" .format(a, b, sub(a, b)))
+
+                                                                                    elif sign == '*':
+                                                                                                    print("{} * {} = {}" .format(a, b, mul(a, b)))
+
+                                                                                    elif sign == '/':
+                                                                                                    print("{} / {} = {}" .format(a, b, div(a, b)))
