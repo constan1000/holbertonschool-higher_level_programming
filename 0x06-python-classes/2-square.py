392a47883cf73d3543c00d28daa79f721a78ae81
constan1000 <hrairimejdi99@gmail.com>
2021-01-12 16:05:44 +0100

    2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,12 +3,12 @@
 
 
 class Square:
-                """ New item SIZEEEE """
-                    def __init__(self, size=0):
-                                        """ Initialize the size ? please ?
+    """ New item SIZEEEE """
+    def __init__(self, size=0):
+        """ Initialize the size ? please ?
         Do verification also please"""
-                                                if type(size) != int:
-                                                                        raise TypeError("size must be an integer")
-                                                elif size < 0:
-                                                                        raise ValueError("size must be >= 0")
-                                                                    self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
