6217652417d24bbf102c8c19253f2aef135c354b
constan1000 <hrairimejdi99@gmail.com>
2021-01-12 16:06:14 +0100

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-""" Class square """
+""" Defining a Square class contains The size ? """
 
 
 class Square:
-        """ Initialize"""
+    """ New item SIZEEEE """
+    def __init__(self, size=0):
+        """ Initialize the size ? please ?
+        Do verification also please"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-            def __init__(self, size=0):
-                        if type(size) != int:
-                                        raise TypeError("size must be an integer")
-                                            if size < 0:
-                                                            raise ValueError("size must be >= 0")
-                                                                self.__size = size
-
-                                                                    """Define square area """
-
-                                                                        def area(self):
-                                                                                    area = self.__size ** 2
-                                                                                            return area
+    def area(self):
+        """ Calculate the area """
+        return self.__size * self.__size
