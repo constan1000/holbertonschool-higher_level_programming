1692fb6b64c2c6164d0da2eb667c77a6a6ccdb2b
constan1000 <hrairimejdi99@gmail.com>
2021-06-25 11:40:21 +0100

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-const FileName = process.argv[2];
-fs.readFile(FileName, 'utf8', function(err, line) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(line);
-    }
+const f = require('fs');
+f.readFile(process.argv[2], 'utf8', function(error, content) {
+    console.log(error || content);
 });
