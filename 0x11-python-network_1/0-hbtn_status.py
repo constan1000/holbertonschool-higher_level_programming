ce5f4c5e1072c72f73af9c942d17decef56e0e53
constan1000 <hrairimejdi99@gmail.com>
2021-06-29 11:11:20 +0100

    0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status"""
-
-
+"""
+Fetches https://intranet.hbtn.io/status
+"""
 if __name__ == "__main__":
     import urllib.request
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        data = response.read()
-
-    data_type = type(data)
-    data_utf8 = data.decode('utf8')
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
 
     print("Body response:")
-    print("\t- type: {}".format(data_type))
-    print("\t- content: {}".format(data))
-    print("\t- utf8 content: {}".format(data_utf8))
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
