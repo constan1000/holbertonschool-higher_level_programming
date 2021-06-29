04f7a2d842b65570ef5bb4646cda60a5f56c1459
constan1000 <hrairimejdi99@gmail.com>
2021-06-29 11:02:31 +0100

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./100-data').list;
-let i = 0;
-const m = list.map(x => x * i++);
-console.log(list);
-console.log(m);
+const oldlist = require('./100-data').list;
+const newlist = oldlist.map((item, index) => item * index);
+console.log(oldlist);
+console.log(newlist);
