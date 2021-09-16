0ef0574ccc762c67b8a68a28b613415eaac00839
constan1000 <hrairimejdi99@gmail.com>
2021-09-16 14:36:48 +0100

    6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-const MyArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < 3; i++) {
-    console.log(MyArray[i]);
+'use strict';
+let args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let arg in args) {
+    console.log(args[arg]);
 }
